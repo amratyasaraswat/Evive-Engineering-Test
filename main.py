33897aa55ad3f111ea59a268d407e30248317663
Amratya Saraswat <amratyasaraswat@gmail.com>
2021-05-25 13:15:17 -0500

    Updated and tested apis

--- main.py
+++ main.py
@@ -19,21 +19,19 @@ class Breakfast(Resource):
             breakfast_list[i] = int(breakfast_list[i])
         if (breakfast_main == 0 or breakfast_side == 0):
             breakfast_order = "Unable to process order: Since Main or Side is missing"
-        if (breakfast_main > 1 or breakfast_side > 1):
+        elif (breakfast_main > 1 or breakfast_side > 1):
             breakfast_order = "Unable to process order: Main and Side can only be served once"
-        if (breakfast_drink == 0):
+        elif (breakfast_drink == 0):
             breakfast_order = breakfast[1] +", "+  breakfast[2] +", Water"
-        if (breakfast_drink == 1):
+        elif (breakfast_drink == 1):
             breakfast_order = breakfast[1] + ", "+ breakfast[2] +", "+ breakfast[3]
-        if (breakfast_drink > 1):
+        elif (breakfast_drink > 1):
             breakfast_order = breakfast[1] +", "+  breakfast[2] +", "+ breakfast[3] + "(" + str(breakfast_drink) + ")"
         return breakfast_order
 
 class Lunch(Resource):
     def get(self, order):
         lunch_order = ""
-        if (len(order) == 0):
-            lunch_order = "Unable to process: Main is missing, Side is missing"
         lunch_list = order.split(",")
         lunch_main = lunch_list.count("1")
         lunch_side = lunch_list.count("2")
@@ -48,20 +46,60 @@ class Lunch(Resource):
             lunch_order = lunch[1] + ", "+ lunch[2] +", Water"
         if (lunch_side > 1 and lunch_drink == 0):
             lunch_order = lunch[1] + ", "+ lunch[2] + "(" + str(lunch_side) + ")"+", Water"   
+        if (lunch_side > 1 and lunch_drink == 1):
+            lunch_order = lunch[1] + ", "+ lunch[2] + "(" + str(lunch_side) + ")"+ lunch[3]   
         if (lunch_main == 1 and lunch_side == 1 and lunch_drink == 1):
             lunch_order = lunch[1] + ", " + lunch[2] + ", " + lunch[3]
+        if (lunch_drink > 1):
+            lunch_order = "Unable to process: Drink cannot be ordered more than once"
         return lunch_order
 
 class Dinner(Resource):
     def get(self, order):
         dinner_list = order.split(",")
-        # print("Dinner item are: " + dinner_list)
-        return dinner[1] +" "+  dinner[2] +" "+ dinner[3] +" " + dinner[4]
+        dinner_order = ""
+        dinner_list = order.split(",")
+        dinner_main = dinner_list.count("1")
+        dinner_side = dinner_list.count("2")
+        dinner_drink = dinner_list.count("3")
+        dinner_dessert = dinner_list.count("4")
+        for i in range (0, len(dinner_list)):
+                dinner_list[i] = int(dinner_list[i])
+        if (dinner_dessert == 0):
+            dinner_order = " Unable to process: Dessert is missing" 
+        if (dinner_main == 0 or dinner_side == 0):
+            dinner_order = "Unable to process order: Since Main or Side is missing"
+        if (dinner_dessert == 0):
+            dinner_order = "Unable to process: Dessert is missing"
+        if (dinner_main == 1 and dinner_side == 1 and dinner_drink == 1 and dinner_dessert ==1):
+            dinner_order = dinner[1] + ", " + dinner[2] + ", " + dinner[3] + ", Water, " + dinner[4]
+        if (dinner_drink > 1 and dinner_dessert == 1):
+            dinner_order = dinner[1] + ", " + dinner[2] + ", " + dinner[3] + "("+ str(dinner_drink)+ "), Water, " + dinner[4]
+        if (dinner_drink == 1 and dinner_dessert > 1):
+            dinner_order = dinner[1] + ", " + dinner[2] + ", " + dinner[3] + ", Water, " + dinner[4] + "(" + str(dinner_dessert) + ")"
+        if (dinner_drink > 1 and dinner_dessert > 1):
+            dinner_order = dinner[1] + ", " + dinner[2] + ", " + dinner[3] + "("+ str(dinner_drink)+ "), Water, " + dinner[4] + "(" + str(dinner_dessert) + ")"
+        return dinner_order
+
+class MissingBreakfastOrder(Resource):
+    def get(self):
+        return "Unable to process: Main is missing, side is missing"
+
+class MissingLunchOrder(Resource):
+    def get(self):
+        return "Unable to process: Main is missing, side is missing"
+
+class MissingDinnerOrder(Resource):
+    def get(self):
+        return "Unable to process: Main is missing, side is missing"
 
 
 api.add_resource(Breakfast, "/Breakfast "+ "<string:order>")
 api.add_resource(Lunch, "/Lunch " + "<string:order>")
 api.add_resource(Dinner, "/Dinner " + "<string:order>")
+api.add_resource(MissingBreakfastOrder, "/Breakfast")
+api.add_resource(MissingLunchOrder, "/Lunch")
+api.add_resource(MissingDinnerOrder, "/Dinner")
 
 if __name__=="__main__":
     app.run(debug = True)
--- test_script.py
+++ test_script.py
@@ -2,6 +2,27 @@ import requests
 
 localHost = "http://127.0.0.1:5000/"
 
-response = requests.get(localHost + "Lunch ")
+# Test Cases for Breakfast
+breakfast_test1 = "Breakfast 1,2,3"
+breakfast_test2 = "Breakfast 2,3,1"
+breakfast_test3 = "Breakfast 1,2,3,3,3"
+breakfast_test4 = "Breakfast 1"
+breakfast_test5 = "Breakfast"
+
+# Test Cases for lunch
+lunch_test1 = "Lunch 1,2,3"
+lunch_test2 = "Lunch 1,2"
+lunch_test3 = "Lunch 1,1,2,3"
+lunch_test4 = "Lunch 1,2,2"
+lunch_test5 = "Lunch"
+
+# Test Cases for Dinner
+dinner_test1 = "Dinner 1,2,3,4"
+dinner_test2 = "Dinner 1,2,3"
+dinner_test3 = "Dinner 4"
+dinner_test4 = "Dinner 1,2,2"
+dinner_test5 = "Dinner 1,2,3,3,4,4"
+
+response = requests.get(localHost + breakfast_test1)
 
 print (response.text)
